2ac2685544449aa7fde0161b826360f5e706d09d
Giuseppe Base <g.base@itsrizzoli.it>
2024-01-11 15:31:29 +0100

    primo commit, il file funzioni.py contiene le query funzioni usate nel main, il file schema contiene come abbiamo strutturato il db e il main contiene l'interfaccia dell'applicazione

---
+++ funzioni.py
@@ -0,0 +1,26 @@
+def ottieni_piste_difficolta() -> list or bool:
+    # funzione che trova tutte le piste in ordine di difficoltà.
+    # restituisce una lista già ordinata in maniera crescente per difficoltà delle piste
+    # restituisce falso se non trova nulla
+    ...
+
+
+def ottieni_piste_aperte() -> list or bool:
+    # funzione che trova tutte le piste aperte.
+    # restituisce una lista
+    # restituisce falso se non trova nulla
+    ...
+
+
+def ottieni_percorso_breve(partenza, arrivo) -> list or bool:
+    # funzione che trova il percorso più breve tra due punti
+    # restituisce una lista di piste in ordine di percorso
+    # restituisce falso se non trova nulla
+    ...
+
+
+def ottieni_percorso_facile(partenza, arrivo) -> list or bool:
+    # funzione che trova il percorso più facile tra due punti
+    # restituisce una lista di piste in ordine di percorso
+    # restituisce falso se non trova nulla
+    ...
